894c01408b63a074bad3e847ba24377ba70f2646
Tum Chaturapruek <tum_chaturapruek@hmc.edu>
2013-08-25 18:19:10 -0700

    Delete home. Replace with the network path.

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Tryloop::Application.routes.draw do
 
   get "static_pages/help"
 
-  root                to: 'static_pages#home'
+  root                to: 'static_pages#network'
   match '/help',      to: 'static_pages#help'
   match '/network',   to: 'static_pages#network'
   match '/search',    to: 'static_pages#search'
